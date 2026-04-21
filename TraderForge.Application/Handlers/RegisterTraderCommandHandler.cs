fbba5f8f0c07dc27971ca94ea407dd129f7bb1df
Lukas Trillat <lstrillat@gmail.com>
2026-04-21 12:35:32 -0400

    feature: added ItraderRepository

--- TraderForge.Application/Handlers/RegisterTraderCommandHandler.cs
+++ TraderForge.Application/Handlers/RegisterTraderCommandHandler.cs
@@ -1,3 +1,6 @@
+using System.Runtime.InteropServices.JavaScript;
+using TraderForge.Application.DTOs;
+using TraderForge.Domain.Entities;
 using TraderForge.Domain.Interfaces;
 
 namespace TraderForge.Application.Handlers;
@@ -5,9 +8,21 @@ namespace TraderForge.Application.Handlers;
 public class RegisterTraderCommandHandler
 {
     public IIdentityService IdentityService;
+    public ITraderRepository TraderRepository;
 
-    RegisterTraderCommandHandler(IIdentityService identityService)
+    RegisterTraderCommandHandler(IIdentityService identityService, ITraderRepository traderRepository)
     {
         IdentityService = identityService;
+        TraderRepository = traderRepository;
     }
+
+    public void Handle(RegisterTraderCommand command)
+    {
+        IdentityService.RegisterUserAsync(command.Email, command.Password);
+        Trader newTrader = new Trader();
+        newTrader.FreeTrialRegistrationDate = DateTime.UtcNow;
+        newTrader.FreeTrialExpirationDate = DateTime.UtcNow.AddDays(7);
+        TraderRepository.AddAsync(newTrader);
+    }
+    
 }
