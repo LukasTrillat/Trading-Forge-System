e5a5cfcb6c8f415c97fa44e3aea71d474ec5a6a2
Ignacio Loncón <ignacioloncon2006@gmail.com>
2026-05-10 20:38:54 -0400

    feat: more supported symbols added

--- TraderForge.Domain/Constants/SupportedAssets.cs
+++ TraderForge.Domain/Constants/SupportedAssets.cs
@@ -4,10 +4,32 @@ public static class SupportedAssets
 {
     public static readonly string[] Symbols =
     {
-        "BTCUSDT",
-        "ETHUSDT",
-        "SOLUSDT",
-        "BNBUSDT",
-        "XRPUSDT",
+        "BTCUSDT",    // Bitcoin
+        "ETHUSDT",    // Ethereum
+        "BNBUSDT",    // BNB
+        "SOLUSDT",    // Solana
+        "XRPUSDT",    // Ripple
+        "ADAUSDT",    // Cardano
+        "AVAXUSDT",   // Avalanche
+        "DOTUSDT",    // Polkadot
+        "TRXUSDT",    // TRON
+        "LINKUSDT",   // Chainlink
+        "TAOUSDT",    // Bittensor
+        "RENDERUSDT", // Render
+        "OPUSDT",     // Optimism
+        "ARBUSDT",    // Arbitrum
+        "IMXUSDT",    // Immutable
+        "STRKUSDT",   // Starknet
+        "SUIUSDT",    // Sui
+        "APTUSDT",    // Aptos
+        "SEIUSDT",    // Sei
+        "UNIUSDT",    // Uniswap
+        "AAVEUSDT",   // Aave
+        "MKRUSDT",    // Maker
+        "DOGEUSDT",   // Dogecoin
+        "PEPEUSDT",   // Pepe
+        "FLOKIUSDT",  // Floki
+        "LTCUSDT",    // Litecoin
+        "ATOMUSDT"    // Cosmos
     };
 }
